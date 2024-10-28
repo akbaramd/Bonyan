3a71e0b7894e49eaa0898233d51e77ea405c670a
ahmadi <akbarsafari00@gmail.com>
2024-10-28 14:29:16 +0330

    .

---
+++ Templates/BonyanTemplate.Infrastructure/Migrations/20241028102603_Init2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BonyanTemplate.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Authors",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Authors");
+        }
+    }
+}
