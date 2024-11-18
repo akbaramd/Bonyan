95bd4db1d50872c36d8f8689c1e1d2795f2bb5f9
ahmadi <akbarsafari00@gmail.com>
2024-11-19 00:46:51 +0330

    1

---
+++ Modules/Bonyan.IdentityManagement/Src/Bonyan.IdentityManagement/Bonyan/IdentityManagement/BonIdentityManagementModule.cs
@@ -0,0 +1,18 @@
+﻿using Bonyan.Modularity;
+using Bonyan.Modularity.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bonyan.IdentityManagement;
+
+public class BonIdentityManagementModule : BonModule
+{
+    public override Task OnConfigureAsync(BonConfigurationContext context)
+    {
+        context.Services.AddAuthentication(c =>
+        {
+            c.DefaultScheme = "";
+        });
+        
+        return base.OnConfigureAsync(context);
+    }
+}
