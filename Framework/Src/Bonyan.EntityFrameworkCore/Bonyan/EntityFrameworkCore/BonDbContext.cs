42d00f62f34c25145c281ac0ec660a4d778c6fd4
ahmadi <akbarsafari00@gmail.com>
2024-12-12 08:38:47 +0330

    .

--- Framework/Src/Bonyan.EntityFrameworkCore/Bonyan/EntityFrameworkCore/BonDbContext.cs
+++ Framework/Src/Bonyan.EntityFrameworkCore/Bonyan/EntityFrameworkCore/BonDbContext.cs
@@ -14,157 +14,173 @@ using Microsoft.Extensions.Options;
 
 namespace Bonyan.EntityFrameworkCore;
 
-public class BonDbContext<TDbContext> : DbContext , IBonDbContext<TDbContext> where TDbContext: DbContext
+public class BonDbContext<TDbContext> : DbContext, IBonDbContext<TDbContext> where TDbContext : DbContext
 {
-  
-  public IBonLazyServiceProvider? ServiceProvider { get; set; } = default!;
-  
-  public BonDbContext(DbContextOptions<TDbContext> options):base(options)
-  {
-    
-  }
-  
-  
-  public IBonCurrentTenant? CurrentTenant => ServiceProvider?.GetService<IBonCurrentTenant>();
-  private IOptions<BonMultiTenancyOptions>? TenancyOptions => ServiceProvider?.GetService<IOptions<BonMultiTenancyOptions>>();
-  private IBonDomainEventDispatcher? DomainEventDispatcher => ServiceProvider?.GetService<IBonDomainEventDispatcher>();
-  protected virtual Guid? CurrentTenantId => CurrentTenant?.Id;
+    public IBonLazyServiceProvider? ServiceProvider { get; set; } = default!;
 
-
-  private static readonly MethodInfo ConfigureBasePropertiesMethodInfo
-    = typeof(BonDbContext<TDbContext>)
-      .GetMethod(
-        nameof(ConfigureBaseProperties),
-        BindingFlags.Instance | BindingFlags.NonPublic
-      )!;
-  
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-    base.OnModelCreating(modelBuilder);
-    
-    foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+    public BonDbContext(DbContextOptions<TDbContext> options) : base(options)
     {
-      ConfigureBasePropertiesMethodInfo
-        .MakeGenericMethod(entityType.ClrType)
-        .Invoke(this, new object[] { modelBuilder, entityType });
     }
-  }
 
-  public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
-  {
-    var entries = ChangeTracker.Entries()
-      .Where(e => e.Entity is IBonAggregateRoot )
-      .ToList();
+    public IBonCurrentTenant? CurrentTenant => ServiceProvider?.GetService<IBonCurrentTenant>();
 
-    foreach (var entry in entries)
+    private IOptions<BonMultiTenancyOptions>? TenancyOptions =>
+        ServiceProvider?.GetService<IOptions<BonMultiTenancyOptions>>();
+
+    private IBonDomainEventDispatcher? DomainEventDispatcher =>
+        ServiceProvider?.GetService<IBonDomainEventDispatcher>();
+
+    protected virtual Guid? CurrentTenantId => CurrentTenant?.Id;
+
+    private static readonly MethodInfo ConfigureBasePropertiesMethodInfo
+        = typeof(BonDbContext<TDbContext>)
+            .GetMethod(
+                nameof(ConfigureBaseProperties),
+                BindingFlags.Instance | BindingFlags.NonPublic
+            )!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-      if (entry.State == EntityState.Added && entry.Entity is IBonCreationAuditable creationAuditableEntity)
-      {
-        creationAuditableEntity.CreatedDate  = DateTime.UtcNow;;
-      }
+        base.OnModelCreating(modelBuilder);
 
-      if (entry.State == EntityState.Modified)
-      {
-        if (entry.Entity is IBonModificationAuditable updateAuditableEntity)
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
-          updateAuditableEntity.ModifiedDate = DateTime.UtcNow;
+            ConfigureBasePropertiesMethodInfo
+                .MakeGenericMethod(entityType.ClrType)
+                .Invoke(this, new object[] { modelBuilder, entityType });
+        }
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is IBonAggregateRoot)
+            .ToList();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added && entry.Entity is IBonCreationAuditable creationAuditableEntity)
+            {
+                creationAuditableEntity.CreatedDate = DateTime.UtcNow;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                if (entry.Entity is IBonModificationAuditable updateAuditableEntity)
+                {
+                    updateAuditableEntity.ModifiedDate = DateTime.UtcNow;
+                }
+
+                if (entry.Entity is IBonSoftDeleteAuditable softDeletableEntity)
+                {
+                    if (softDeletableEntity.IsDeleted)
+                    {
+                        softDeletableEntity.DeletedDate = DateTime.UtcNow;
+                    }
+                    else
+                    {
+                        entry.Property(nameof(IBonSoftDeleteAuditable.DeletedDate)).IsModified = false;
+                        entry.Property(nameof(IBonSoftDeleteAuditable.IsDeleted)).IsModified = false;
+                    }
+                }
+            }
         }
 
-        if (entry.Entity is IBonSoftDeleteAuditable softDeletableEntity)
+        // Collect domain events
+        var domainEvents = ChangeTracker.Entries()
+            .Where(e => e.Entity is IBonAggregateRoot aggregateRoot && aggregateRoot.DomainEvents.Any())
+            .SelectMany(e => ((IBonAggregateRoot)e.Entity).DomainEvents)
+            .ToList();
+
+        // Save changes to the database
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        // Publish domain events after the save is successful
+        if (DomainEventDispatcher != null)
         {
-          if (softDeletableEntity.IsDeleted)
-          {
-            softDeletableEntity.DeletedDate = DateTime.UtcNow;
-          }
-          else
-          {
-            entry.Property(nameof(IBonSoftDeleteAuditable.DeletedDate)).IsModified = false;
-            entry.Property(nameof(IBonSoftDeleteAuditable.IsDeleted)).IsModified = false;
-          }
+            foreach (var domainEvent in domainEvents)
+            {
+                await DomainEventDispatcher.DispatchAsync(domainEvent, cancellationToken);
+            }
         }
-      }
 
-      if (DomainEventDispatcher == null || entry.Entity is not IBonAggregateRoot agge) continue;
-      
-      foreach (var @event in agge.DomainEvents)
-      {
-        await DomainEventDispatcher.DispatchAsync(@event, cancellationToken);
-      }
+        return result;
     }
 
-    
-    
-    return await base.SaveChangesAsync(cancellationToken);
-  }
-
-  public Task<int> SaveChangesOnDbContextAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
-  {
-    return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-  }
-
-  
-  protected virtual void ConfigureBaseProperties<TEntity>(ModelBuilder modelBuilder, IMutableEntityType mutableEntityType)
-    where TEntity : class
-  {
-    if (mutableEntityType.IsOwned())
+    public Task<int> SaveChangesOnDbContextAsync(bool acceptAllChangesOnSuccess,
+        CancellationToken cancellationToken = default)
     {
-      return;
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
     }
 
-    if (!typeof(IBonEntity).IsAssignableFrom(typeof(TEntity)))
+    protected virtual void ConfigureBaseProperties<TEntity>(ModelBuilder modelBuilder,
+        IMutableEntityType mutableEntityType)
+        where TEntity : class
     {
-      return;
+        if (mutableEntityType.IsOwned())
+        {
+            return;
+        }
+
+        if (!typeof(IBonEntity).IsAssignableFrom(typeof(TEntity)))
+        {
+            return;
+        }
+
+        modelBuilder.Entity<TEntity>().ConfigureByConvention();
+
+        ConfigureGlobalFilters<TEntity>(modelBuilder, mutableEntityType);
     }
 
-    modelBuilder.Entity<TEntity>().ConfigureByConvention();
-
-    ConfigureGlobalFilters<TEntity>(modelBuilder, mutableEntityType);
-  }
-  
-  protected virtual void ConfigureGlobalFilters<TEntity>(ModelBuilder modelBuilder, IMutableEntityType mutableEntityType)
-    where TEntity : class
-  {
-    if (mutableEntityType.BaseType == null && ShouldFilterEntity<TEntity>(mutableEntityType))
+    protected virtual void ConfigureGlobalFilters<TEntity>(ModelBuilder modelBuilder,
+        IMutableEntityType mutableEntityType)
+        where TEntity : class
     {
-      var filterExpression = CreateFilterExpression<TEntity>();
-      if (filterExpression != null)
-      {
-        modelBuilder.Entity<TEntity>().HasBonyanQueryFilter(filterExpression);
-      }
+        if (mutableEntityType.BaseType == null && ShouldFilterEntity<TEntity>(mutableEntityType))
+        {
+            var filterExpression = CreateFilterExpression<TEntity>();
+            if (filterExpression != null)
+            {
+                modelBuilder.Entity<TEntity>().HasBonyanQueryFilter(filterExpression);
+            }
+        }
     }
-  }
-  
-  protected virtual bool ShouldFilterEntity<TEntity>(IMutableEntityType entityType) where TEntity : class
-  {
-    if (typeof(IBonMultiTenant).IsAssignableFrom(typeof(TEntity)))
+
+    protected virtual bool ShouldFilterEntity<TEntity>(IMutableEntityType entityType) where TEntity : class
     {
-      return true;
+        if (typeof(IBonMultiTenant).IsAssignableFrom(typeof(TEntity)))
+        {
+            return true;
+        }
+
+        if (typeof(IBonSoftDeleteAuditable).IsAssignableFrom(typeof(TEntity)))
+        {
+            return true;
+        }
+
+        return false;
     }
 
-    if (typeof(IBonSoftDeleteAuditable).IsAssignableFrom(typeof(TEntity)))
+    protected virtual Expression<Func<TEntity, bool>>? CreateFilterExpression<TEntity>()
+        where TEntity : class
     {
-      return true;
+        Expression<Func<TEntity, bool>>? expression = null;
+
+        if (typeof(IBonSoftDeleteAuditable).IsAssignableFrom(typeof(TEntity)))
+        {
+            expression = e => !EF.Property<bool>(e, "IsDeleted");
+        }
+
+        if (TenancyOptions != null && TenancyOptions is { Value.IsEnabled: true } &&
+            typeof(IBonMultiTenant).IsAssignableFrom(typeof(TEntity)))
+        {
+            Expression<Func<TEntity, bool>>
+                multiTenantFilter = e => EF.Property<Guid>(e, "TenantId") == CurrentTenantId;
+            expression = expression == null
+                ? multiTenantFilter
+                : BonQueryFilterExpressionHelper.CombineExpressions(expression, multiTenantFilter);
+        }
+
+        return expression;
     }
-
-    return false;
-  }
-
-  protected virtual Expression<Func<TEntity, bool>>? CreateFilterExpression<TEntity>()
-    where TEntity : class
-  {
-    Expression<Func<TEntity, bool>>? expression = null;
-
-    if (typeof(IBonSoftDeleteAuditable).IsAssignableFrom(typeof(TEntity)))
-    {
-      expression = e =>  !EF.Property<bool>(e, "IsDeleted");
-    }
-    
-    if (TenancyOptions != null && TenancyOptions is { Value.IsEnabled: true } && typeof(IBonMultiTenant).IsAssignableFrom(typeof(TEntity)))
-    {
-      Expression<Func<TEntity, bool>> multiTenantFilter = e =>     EF.Property<Guid>(e, "TenantId") == CurrentTenantId;
-      expression = expression == null ? multiTenantFilter : BonQueryFilterExpressionHelper.CombineExpressions(expression, multiTenantFilter);
-    }
-
-    return expression;
-  }
 }
